41979a27adaaefa593271f7f59ebc823ef125298
Dário Nascimento <dfrnascimento@gmail.com>
2013-08-20 15:16:37 +0100

    migracao para apache httpCore - not working

--- src/pt/inesc/UndoServer.java
+++ src/pt/inesc/UndoServer.java
@@ -3,35 +3,21 @@ package pt.inesc;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.SocketAddress;
-import java.net.UnknownHostException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class UndoServer
-        implements Runnable {
-    private int clientPort;
+import com.sun.net.httpserver.HttpServer;
+
+public class UndoServer {
+    private int clientSidePort;
     private String destinationHost;
     private int destinationPort;
 
-    private Boolean isStopped = false;
-    private ServerSocket serverSocket = null;
-
     private static Logger log = LogManager.getLogger("UndoServer");
 
-
-
-
-
-
-
-
-
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         log = LogManager.getLogger("UndoServer");
         UndoServer server = new UndoServer(9000, "localhost", 8080);
         server.run();
@@ -39,68 +25,25 @@ public class UndoServer
 
     public UndoServer(int clientPort, String destinationHost, int destinationPort) {
         super();
-        this.clientPort = clientPort;
+        clientSidePort = clientPort;
         this.destinationHost = destinationHost;
         this.destinationPort = destinationPort;
     }
 
 
-    @Override
-    public void run() {
+    public void run() throws IOException {
         // Create socket to real server
         InetAddress inteAddress;
-        try {
-            inteAddress = InetAddress.getByName(destinationHost);
-            SocketAddress destinationAddress = new InetSocketAddress(inteAddress,
-                    destinationPort);
+        inteAddress = InetAddress.getByName(destinationHost);
+        SocketAddress destinationAddress = new InetSocketAddress(inteAddress,
+                destinationPort);
 
+        HttpServer server = HttpServer.create(new InetSocketAddress("", clientSidePort),
+                                              clientSidePort);
 
-            openServerSocket();
-            while (!isStopped()) {
-                Socket clientSocket = null;
-                try {
-                    clientSocket = serverSocket.accept();
-                } catch (IOException e) {
-                    if (isStopped()) {
-                        System.out.println("Server Stopped.");
-                        return;
-                    }
-                    throw new RuntimeException("Error accepting client connection", e);
-                }
-                new Thread(new WorkerRunnable(clientSocket, destinationAddress)).start();
-            }
-
-        } catch (UnknownHostException e1) {
-            log.error("Destination host does not exisit: " + destinationHost);
-        }
-
-    }
-
-    private synchronized boolean isStopped() {
-        return isStopped;
-    }
-
-
-
-
-    public synchronized void stop() {
-        isStopped = true;
-        try {
-            serverSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Error closing server", e);
-        }
-    }
-
-
-
-    private void openServerSocket() {
-        try {
-            serverSocket = new ServerSocket(clientPort);
-            log.info("Proxy started at port: " + clientPort);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot open port " + clientPort, e);
-        }
+        server.createContext("/", new UndoHandler(destinationAddress));
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 
 }
