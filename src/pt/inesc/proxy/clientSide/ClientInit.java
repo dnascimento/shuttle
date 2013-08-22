b0cbd2785c1f22a56f45b8af105604289cfb3d4f
Dário Nascimento <dfrnascimento@gmail.com>
2013-08-22 16:45:30 +0100

    BUG: Netty splits requests due to buffer size

--- src/pt/inesc/proxy/clientSide/ClientInit.java
+++ src/pt/inesc/proxy/clientSide/ClientInit.java
@@ -3,6 +3,7 @@ package pt.inesc.proxy.clientSide;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
 
 
 
@@ -24,7 +25,7 @@ public class ClientInit extends
     @Override
     public void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline p = ch.pipeline();
-        // p.addLast("codec", new HttpServerCodec());
+        p.addLast("codec", new HttpServerCodec());
         p.addLast("proxy", new ProxyHandler(remoteHost, remotePort));
         // p.addLast(new HTTPHandler(remoteHost, remotePort));
     }
