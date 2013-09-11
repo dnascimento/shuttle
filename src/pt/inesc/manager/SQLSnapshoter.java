3f66a06e96f9c0f259cb5735a7f2be41ebe181cb
Dário Nascimento <dfrnascimento@gmail.com>
2013-09-11 10:05:13 +0300

    MySQL with log. Test Version - Its not the best solution

--- src/pt/inesc/manager/SQLSnapshoter.java
+++ src/pt/inesc/manager/SQLSnapshoter.java
@@ -29,14 +29,16 @@ public class SQLSnapshoter
     @Override
     public LinkedList<String> shot(Integer id) throws SQLException {
         LinkedList<String> snapshot = new LinkedList<String>();
-        try {
+        Statement clean = conn.createStatement();
+        clean.execute("truncate Pages_temp");
 
+        try {
             // Simulate row by row snapshot
             // Set new entry on log
             Statement stat1 = conn.createStatement();
             ResultSet rtimes = stat1.executeQuery("select max(event_time) from mysql.general_log;");
             rtimes.next();
-            String lastLogEntry = rtimes.getString(0);
+            String lastLogEntry = rtimes.getString(1);
 
             // Get all URL
             Statement query = conn.createStatement();
@@ -52,7 +54,7 @@ public class SQLSnapshoter
                 ResultSet urls = stat.executeQuery();
 
                 while (urls.next()) {
-                    stat = conn.prepareStatement("insert into Pages(url,author,title,date,content,id,snap) values(?,?,?,?,?,?,?)");
+                    stat = conn.prepareStatement("insert into Pages_temp(url,author,title,date,content,id,snap) values(?,?,?,?,?,?,?)");
                     stat.setString(1, urls.getString("url"));
                     stat.setString(2, urls.getString("author"));
                     stat.setString(3, urls.getString("title"));
@@ -72,34 +74,58 @@ public class SQLSnapshoter
             }
             rs.close();
 
+            // Get the biggest ID of snap
+            stat1 = conn.createStatement();
+            rtimes = stat1.executeQuery("select max(id) from Pages_temp;");
+            rtimes.next();
+            int biggestID = rtimes.getInt(1);
 
-            // Read the log
+            // TODO Wait for biggest ID of snap responsed
+
+
+            // Get Log
             PreparedStatement stat3 = conn.prepareStatement("select * from mysql.general_log where event_time >= ?");
             stat3.setString(1, lastLogEntry);
             ResultSet pendentLog = stat3.executeQuery();
 
+
+            // Program is snaped, log is copy
+
+
+
+            // Apply log only on ID lowers than biggest ID
+            // TODO Problem: how to decide which are lower? SQL can move multiple lines
+            // each transaction.
+            // TODO This system just works with document based, 1 row each time.
+
+
             while (pendentLog.next()) {
-                String command = pendentLog.getString("argument");
-                System.out.println(command);
-                snapshot.add(command);
+                String op = pendentLog.getString("argument");
+                // Remove all queries due to data copy
+                if (!op.startsWith("insert into Pages_temp")
+                        && (op.startsWith("insert") || op.startsWith("update"))) {
+                    continue;
+                }
+
+                // TODO Check-id
+                Statement pendentOp = conn.createStatement();
+                pendentOp.execute(op);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return snapshot;
     }
 
     @Override
     public void load(LinkedList<String> pendentOperations, int id) throws SQLException {
         // Delete all versions more recent than snapshot
-        PreparedStatement stat = conn.prepareStatement("delete FROM Pages WHERE snap <> ?");
-        stat.setInt(1, id);
-        stat.execute();
+        Statement stat = conn.createStatement();
+        stat.execute("truncate Pages");
 
-        String op;
-        while ((op = pendentOperations.getFirst()) != null) {
-            Statement pendentOp = conn.createStatement();
-            pendentOp.execute(op);
-        }
+        // Copy the data from temp to actual
+        Statement stat2 = conn.createStatement();
+        stat2.execute("INSERT INTO Pages (url, author, title, date, content, id,snap) SELECT url, author, title, date, content, id, snap FROM Pages_temp");
     }
 }
